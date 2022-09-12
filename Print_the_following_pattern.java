7c90fba3f5dd78ca6c882d2429d873d13bf2a41d
anitha-0203 <112751429+anitha-0203@users.noreply.github.com>
2022-09-12 20:45:50 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int rows=sc.nextInt();         for(int i=1;i<=rows;i++)         {             for(int j=1;j<i;j++)             {
                }
                {
                    for(int j=i;j<=rows;j++)
                    {
                        System.out.print(j+" ");
                    }
                    System.out.println();
                }
                sc.close();
            }
        }
    }

---
+++ Print_the_following_pattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int rows=sc.nextInt();
+        for(int i=1;i<=rows;i++)
+        {
+            for(int j=1;j<i;j++)
+            {
+                
+            }
+            {
+                for(int j=i;j<=rows;j++)
+                {
+                    System.out.print(j+" ");
+                }
+                System.out.println();
+            }
+            sc.close();
+        }
+    }
+}
