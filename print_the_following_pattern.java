4c36a25a7caa0ed8a524a79c45dba1034a817c71
anitha-0203 <112751429+anitha-0203@users.noreply.github.com>
2022-09-13 20:50:37 +0530

    import java.util.*; class Pattern {    public static void main(String args[])    {        int i,j,n;         Scanner sc=new Scanner(System.in);         //System.out.println("Enter the no of lines");         n=sc.nextInt();         for(i=n;i>=1;i--)         {              for(j=1;j<=i;j++)              {                   System.out.print((char)(i+64)+" ");              }              System.out.println("");         }    } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,19 +1,19 @@
-import java.util.Scanner;
+import java.util.*;
 class Pattern
 {
-    public static void main(String args[])
-    {
+   public static void main(String args[])
+   {
+       int i,j,n;
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        System.out.print("");
-        for(int i=1;i<=n;i++)
+        //System.out.println("Enter the no of lines");
+        n=sc.nextInt();
+        for(i=n;i>=1;i--)
         {
-            for(int j=1;j<=i;j++)
-            {
-                System.out.print(j+"");
-            }
-            System.out.println();
+             for(j=1;j<=i;j++)
+             {
+                  System.out.print((char)(i+64)+" ");
+             }
+             System.out.println("");
         }
-        sc.close();
-    }
-}
+   }
+} 
